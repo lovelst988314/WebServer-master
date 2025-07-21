1978e583fb163ef3c19484f96471cbe2c8a9cf4a
lovelst988314 <1033211505@qq.com>
2025-07-21 09:45:54 +0800

    epoll_end

--- code/server/epoller.cpp
+++ code/server/epoller.cpp
@@ -1,17 +1,12 @@
-/*
- * @Author       : mark
- * @Date         : 2020-06-19
- * @copyleft Apache 2.0
- */
-
 #include "epoller.h"
 
 Epoller::Epoller(int maxEvent):epollFd_(epoll_create(512)), events_(maxEvent){
     assert(epollFd_ >= 0 && events_.size() > 0);
+    // 确保 epollFd_ 有效且事件列表不为空 
 }
 
 Epoller::~Epoller() {
-    close(epollFd_);
+    close(epollFd_);  // 关闭 epoll 文件描述符
 }
 
 bool Epoller::AddFd(int fd, uint32_t events) {
@@ -19,7 +14,7 @@ bool Epoller::AddFd(int fd, uint32_t events) {
     epoll_event ev = {0};
     ev.data.fd = fd;
     ev.events = events;
-    return 0 == epoll_ctl(epollFd_, EPOLL_CTL_ADD, fd, &ev);
+    return 0 == epoll_ctl(epollFd_, EPOLL_CTL_ADD, fd, &ev);  // 添加描述符。 返回零表示成功
 }
 
 bool Epoller::ModFd(int fd, uint32_t events) {
@@ -27,13 +22,13 @@ bool Epoller::ModFd(int fd, uint32_t events) {
     epoll_event ev = {0};
     ev.data.fd = fd;
     ev.events = events;
-    return 0 == epoll_ctl(epollFd_, EPOLL_CTL_MOD, fd, &ev);
+    return 0 == epoll_ctl(epollFd_, EPOLL_CTL_MOD, fd, &ev); // 修改描述符。 
 }
 
 bool Epoller::DelFd(int fd) {
     if(fd < 0) return false;
     epoll_event ev = {0};
-    return 0 == epoll_ctl(epollFd_, EPOLL_CTL_DEL, fd, &ev);
+    return 0 == epoll_ctl(epollFd_, EPOLL_CTL_DEL, fd, &ev);  // 删除文件描述符
 }
 
 int Epoller::Wait(int timeoutMs) {
@@ -42,10 +37,20 @@ int Epoller::Wait(int timeoutMs) {
 
 int Epoller::GetEventFd(size_t i) const {
     assert(i < events_.size() && i >= 0);
-    return events_[i].data.fd;
+    return events_[i].data.fd;  // 获取就绪事件的文件描述符
 }
 
 uint32_t Epoller::GetEvents(size_t i) const {
     assert(i < events_.size() && i >= 0);
-    return events_[i].events;
+    return events_[i].events;  // 获取就绪事件的类型
 }
+
+
+//  int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event);
+//  epfd：epoll_create() 返回的 epoll 实例文件描述符。
+//  op：操作类型，可选值：
+//  EPOLL_CTL_ADD：注册新的文件描述符到 epfd。
+//  EPOLL_CTL_MOD：修改已注册文件描述符的监听事件。
+//  EPOLL_CTL_DEL：从 epfd 中删除文件描述符，此时 event 参数可传 NULL。
+//  fd：要操作的目标文件描述符（如套接字、管道等）。
+//  event：指向 struct epoll_event 的指针，指定监听的事件类型和关联数据。
--- code/server/epoller.h
+++ code/server/epoller.h
@@ -10,26 +10,26 @@
 
 class Epoller {
 public:
-    explicit Epoller(int maxEvent = 1024);
+    explicit Epoller(int maxEvent = 1024);  //初始化 epoll 文件描述符并设置事件列表大小。
 
-    ~Epoller();
+    ~Epoller();  
 
-    bool AddFd(int fd, uint32_t events);
+    bool AddFd(int fd, uint32_t events);  // 添加文件描述符到 epoll 实例，并设置监听的事件类型。
 
-    bool ModFd(int fd, uint32_t events);
+    bool ModFd(int fd, uint32_t events);  // 修改已添加的文件描述符的事件类型。
 
-    bool DelFd(int fd);
+    bool DelFd(int fd);  // 从 epoll 实例中删除文件描述符。
 
-    int Wait(int timeoutMs = -1);
+    int Wait(int timeoutMs = -1);  // 等待事件发生，返回就绪的事件数量。
 
-    int GetEventFd(size_t i) const;
+    int GetEventFd(size_t i) const;  // 获取就绪事件的文件描述符。
 
-    uint32_t GetEvents(size_t i) const;
+    uint32_t GetEvents(size_t i) const;  // 获取就绪事件的类型。
         
 private:
-    int epollFd_;
+    int epollFd_;  // epoll 文件描述符
 
-    std::vector<struct epoll_event> events_;    
+    std::vector<struct epoll_event> events_;      // 存储就绪事件的 vector
 };
 
-#endif //EPOLLER_H
+#endif 
